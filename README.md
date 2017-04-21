f7592513c7cb70d4459d2004fd51de39bca56245
Dow Drake <ddrake@pdx.edu>
2017-04-20 21:01:17 -0700

    update docs, clean up comparison

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mth653
 Algorithms for linear algebra
+based on Numerical Linear Algebra by Trefethen and Bau
+and notes from PSU Course Math 653 
--- qr/gs_compare.py
+++ qr/gs_compare.py
@@ -1,20 +1,29 @@
 import gsm
 import gs
-
 from numpy import *
-
 from matplotlib import pyplot as plt
 
-[U,X] = linalg.qr(random.rand(80,80))
-[V,X] = linalg.qr(random.rand(80,80))
+"""Experiment 2 in Trefethen & Bau
+   Demonstrates the instability of the Classical Gram-Schmidt
+   algorithm compared with the Modified algorithm
+"""
+m=80
 
-S = diag(2.**(-1-array(range(80))))
+# create mxm unitary matrices U and V
+[U,X] = linalg.qr(random.rand(m,m))
+[V,X] = linalg.qr(random.rand(m,m))
 
+# construct an array of eigenvalues growing exponentially smaller
+S = diag(2.**(-1-array(range(m))))
+
+# construct matrix A with desired eigenvalues
 A = U.dot(S).dot(V)
 
+# compare the Gram-Schmidt algorithms
 QC,RC = gs.gs(A)
 QM,RM = gsm.gsm(A)
 
-plt.semilogy(range(80), diag(RC),'bs',diag(RM),'ro')
+# The classical algorithm fails around the square root of machine epsilon
+plt.semilogy(range(m), diag(RC),'bs',diag(RM),'ro')
 plt.show()
 
