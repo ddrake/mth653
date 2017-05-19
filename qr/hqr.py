15f3326215810fbd13a95d6dc389e6a1143446ad
Dow Drake <ddrake@pdx.edu>
2017-05-18 19:32:53 -0700

    working householder qr

--- qr/hqr.py
+++ qr/hqr.py
@@ -7,12 +7,11 @@ def qr(a):
     (m,n) = shape(a)
     v = zeros((m,n))
     for k in range(n):
-        print("k=%d" % k)
-        x = a[k:m,k]
+        x = a[k:m,k].copy()
         x[0] += sign(x[0])*linalg.norm(x)
         vk = x / linalg.norm(x)
         v[k:m,k] = vk
-        print(vk)
         a[k:m,k:n] -= 2*vk[:,newaxis].dot(vk.conj().dot(a[k:m,k:n])[newaxis,:])
-        print(a)
-    return v
+    return v,a
+
+
