75c593a76110e05291aa9d36850340a0e0df0112
Dow Drake <dowdrake@msn.com>
2017-05-20 13:14:33 -0700

    bugfixes, restructuring, still failing on complex case

--- qr/hqr.py
+++ qr/hqr.py
@@ -9,23 +9,17 @@ Q can be produced if needed using get_q(v).  But often it is enough to
 compute Q*b or Qx so methods are provided for those operations.
 Most production QR algorithms use this approach
 
- 
 >>> a = array([[-2.49952922, -1.03751133, -1.21085266, -1.24115004],
 ...            [-0.80586535,  1.31580494, -0.25698657, -0.09300651],
 ...            [-0.31995424, -0.36042199, -1.18012579, -0.12960803],
 ...            [-0.51933797, -0.29275398,  0.0953217 , -0.32097443],
 ...            [-0.66819142,  0.18847333, -0.43734184, -0.30990116]])
-
 >>> v,r = hqr(a)
-
 >>> linalg.norm(tril(r,-1),'f') < 1.0e-15
 True
-
 >>> q = makeq(v)
-
 >>> linalg.norm(a - q.dot(r),'f') < 1.0e-15
 True
-
 """
 
 from numpy import *
@@ -38,52 +32,80 @@ def hqr(ao):
     Given a matrix, compute the Householder triangularization
     """
     a = ao.copy()
-    (m,n) = shape(a)
-    v = zeros((m,n))
+    m,n = shape(a)
+    v = zeros((m,n), dtype=complex)
     for k in range(n):
-        x = a[k:m,k].copy()
+        x = a[k:,k].copy()
         x[0] += sign(x[0])*linalg.norm(x,2)
         vk = x[:,None] / linalg.norm(x,2)
-        v[k:m,k] = vk[:,0]
-        a[k:m,k:n] -= 2*vk.dot(vk.T.conj().dot(a[k:m,k:n]))
+        v[k:,k] = vk[:,0]
+        # subtract (m-k+1)x(m-k+1) rank 1 submatrix from a
+        a[k:,k:] -= 2*vk.dot(vk.conj().T.dot(a[k:,k:]))
     return v,a
 
 def qhb(v,bo):
     """ 
-    Given the matrix v returned from hqr and a vector (or matrix)
-    b with m rows, compute the product Q*b
+    Given the matrix v returned from hqr and an m-vector b
+    compute the product Q* b
     """
-    m,n = shape(v)
+    _,n = shape(v)
     b = bo.copy()
-    if len(shape(b)) == 1:
-        reshape(b,(m,1))
     for k in range(n):
-        vk = v[k:,k,None]
-        b[k:,:] -= 2*vk.dot(vk.conj().T.dot(b[k:,:]))
+        b[k:] -= 2*v[k:,k]*(v[k:,k].conj().dot(b[k:]))
     return b
+
+def qx(v,xo):
+    """
+    Given the matrix v returned from hqr and a vector 
+    x with m rows, compute the product Q x
+    """
+    _,n = shape(v)
+    x = xo.copy()
+    for k in range(n-1,-1,-1):
+        x[k:] -= 2*v[k:,k]*(v[k:,k].conj().dot(x[k:]))
+    return x
+
+def makeq1(v):
+    """
+    Given the matrix v returned from hqr, form the matrix Q by 
+    computing Q* I and taking the conjugate transpose.
+    """
+    m,_ = shape(v)
+    Q = zeros((m,m),dtype=complex)
+    I = eye(m,dtype=complex)
+    for i in range(m):
+        Q[:,i]=qhb(v,I[:,i])
+    return Q.conj().T
 
 def makeq(v):
     """
     Given the matrix v returned from hqr, form the matrix Q by 
-    computing Q*I and taking the conjugate transpose.
+    computing Q I 
     """
     m,_ = shape(v)
-    return qhb(v,eye(m)).conj().T
+    Q = zeros((m,m),dtype=complex)
+    I = eye(m,dtype=complex)
+    for i in range(m):
+        Q[:,i]=qx(v,I[:,i])
+    return Q
 
-def qx(v,xo):
+
+def bslv(r,c):
     """
-    Given the matrix v returned from hqr and a vector (or matrix)
-    x with m rows, compute the product Qx
+    r should be upper triangular mxn with nonzero diagonal entries 
+    and c mx1.  Solves rx = c by back substitution
     """
-    m,n = shape(v)
-    xo = xo.copy()
-    if len(shape(x)) == 1:
-        reshape(x,(m,1))
-    for k in range(n-1,-1,-1):
-        vk = v[k:,k,None]
-        x[k:,:] -= 2*vk.dot(vk.conj().T.dot(x[k:,:]))
+    _,n = shape(r)
+    x = zeros(n)
+    for i in range(n-1,-1,-1):
+        x[i] = (c[i]-r[i,i+1:].dot(x[i+1:])) / r[i,i]
     return x
 
+def slv(a,bo):
+    v,r = hqr(a)
+    c = qhb(v,b)
+    x = bslv(r,c)
+    return x
 
 if __name__ == "__main__":
     import doctest
